1bb4a90c0076f9196bd3bd8aff575b466c245aa0
Chicho25 <pedroarrieta25@hotmail.com>
2022-05-12 16:33:13 -0500

    con ssh desde local al sever

--- inedx.php
+++ inedx.php
@@ -9,6 +9,6 @@
 <body>
     <h1>Como asi xolo, no esta funka</h1>
     <p>Esto para la rama Simu sinsinsin mas cambios esto ees desde la pagina de github</p>
-    
+    <p>Esto es otro cambio ya por ssh</p>
 </body>
 </html>
